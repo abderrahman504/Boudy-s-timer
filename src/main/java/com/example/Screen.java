6d6fd45b0ca411fc4dde6e1d0f9153b4a099319e
abderrahman504 <rahmankhsherb@gmail.com>
2023-08-21 18:06:12 +0300

    Show extra message in log for how long you worked or were on break

--- src/main/java/com/example/Screen.java
+++ src/main/java/com/example/Screen.java
@@ -5,6 +5,7 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.time.LocalTime;
+import java.time.temporal.ChronoUnit;
 
 
 public class Screen extends JPanel implements ActionListener
@@ -12,6 +13,9 @@ public class Screen extends JPanel implements ActionListener
 	JButton workbutton, breakButton;
 	JPanel logPanel;
 	Color workColor = new Color(139, 149, 208), breakColor = new Color(133, 186, 94);
+	enum Mode {NONE, WORK, BREAK};
+	Mode mode = Mode.NONE;
+	
 	public Screen()
 	{
 		setLayout(new BorderLayout());
@@ -41,27 +45,57 @@ public class Screen extends JPanel implements ActionListener
 
 	void OnBreakPressed()
 	{
+		logPanel.setBackground(breakColor);
 		//Add entry to the log
 		String str = String.format("Took break at %1$tr", LocalTime.now());
 		JLabel entry = new JLabel(str);
 		logPanel.add(entry);
-		logPanel.setBackground(breakColor);
-		// repaint();
+		if (mode == Mode.WORK)
+		{
+			long secondsPassed = App.getStartTime().until(LocalTime.now(), ChronoUnit.SECONDS);
+			long minutesPassed = secondsPassed / 60;
+			long hoursPassed = minutesPassed / 60;
+			secondsPassed %= 60;
+			minutesPassed %= 60;
+			String str2;
+			if(minutesPassed == 0) str2 = String.format("%1$ds", secondsPassed);
+			else if(hoursPassed == 0) str2 = String.format("%2$dm %1$ds", secondsPassed, minutesPassed);
+			else str2 = String.format("%3$dh %2$dm %1$ds", secondsPassed, minutesPassed, hoursPassed);
+			str2 = "Worked for " + str2;
+			entry = new JLabel(str2);
+			logPanel.add(entry);
+		}
 		revalidate();
 		App.mode = "Break";
+		mode = Mode.BREAK;
 		App.setStartTime();
 	}
 	
 	void OnWorkPressed()
 	{
+		logPanel.setBackground(workColor);
 		//Add entry to the log
 		String str = String.format("Started work at %1$tr", LocalTime.now());
 		JLabel entry = new JLabel(str);
 		logPanel.add(entry);
-		logPanel.setBackground(workColor);
-		// repaint();
+		if (mode == Mode.BREAK)
+		{
+			long secondsPassed = App.getStartTime().until(LocalTime.now(), ChronoUnit.SECONDS);
+			long minutesPassed = secondsPassed / 60;
+			long hoursPassed = minutesPassed / 60;
+			secondsPassed %= 60;
+			minutesPassed %= 60;
+			String str2;
+			if(minutesPassed == 0) str2 = String.format("%1$ds", secondsPassed);
+			else if(hoursPassed == 0) str2 = String.format("%2$dm %1$ds", secondsPassed, minutesPassed);
+			else str2 = String.format("%3$dh %2$dm %1$ds", secondsPassed, minutesPassed, hoursPassed);
+			str2 = "On break for " + str2;
+			entry = new JLabel(str2);
+			logPanel.add(entry);
+		}
 		revalidate();
 		App.mode = "Work";
+		mode = Mode.WORK;
 		App.setStartTime();
 	}
 }
