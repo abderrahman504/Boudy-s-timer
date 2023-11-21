c9b7338fbde848af4638131549c9ac05e51230c7
abderrahman504 <rahmankhsherb@gmail.com>
2023-11-21 15:38:18 +0200

    Remove unnecessary imports

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,7 +1,5 @@
 package com.example;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.time.LocalDateTime;
 
 import javax.swing.JFrame;
--- src/main/java/com/example/Screen.java
+++ src/main/java/com/example/Screen.java
@@ -5,7 +5,6 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.time.LocalDateTime;
-import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
 
 
